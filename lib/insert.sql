ad6a2de7efb18f878ecc3b1299777165ea4ab43f
Elchonon <ekn400@gmail.com>
2024-12-18 18:19:19 -0500

    lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,sex,color,temperament,alive) VALUES ('Mr. Chocolate','M','Black','Moody',1);
+INSERT INTO bears (name,sex,color,temperament,alive) VALUES ('Rowdy','M','Black','Moody',1);
+INSERT INTO bears (name,sex,color,temperament,alive) VALUES ('Tabitha','M','Black','Moody',1);
+INSERT INTO bears (name,sex,color,temperament,alive) VALUES ('Sergeant Brown','M','Black','Moody',1);
+INSERT INTO bears (name,sex,color,temperament,alive) VALUES ('Melisa','F','Black','Moody',1);
+INSERT INTO bears (name,sex,color,temperament,alive) VALUES ('Grinch','F','Black','Moody',1);
+INSERT INTO bears (name,sex,color,temperament,alive) VALUES ('Wendy','F','Black','Moody',1);
+INSERT INTO bears (name,sex,color,temperament,alive) VALUES (NULL,'F','Black','Moody',1);
